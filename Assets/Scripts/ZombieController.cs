aefc5c02c84211dac02a79137be955616cb28c88
hunte <besscomdark@gmail.com>
2024-07-10 08:19:55 +0800

    調整

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+    }
+}
